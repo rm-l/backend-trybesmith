488a945c1185cd98f130a7fefd1956b97c70bdf2
Robson Silva <rm8l@outlook.com>
2023-03-07 22:12:47 -0300

    test

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -10,10 +10,10 @@ export default class OrderModel {
 
   public async getAll(): Promise<IOrder[]> {
     const result = await this.connection.execute(
-      `SELECT orders.id, orders.user_id AS userId, JSON_ARRAYAGG(products.id) AS productsIds
-    FROM Trybesmith.orders AS orders
-    INNER JOIN Trybesmith.products AS products
-    ON orders.id = products.order_id
+      `SELECT orders.id, orders.user_id as userId, 
+    JSON_ARRAYAGG(products.id) AS productsIds
+    FROM Trybesmith.orders 
+    INNER JOIN Trybesmith.products ON orders.id = products.order_id
     GROUP BY orders.id`,
     );
     
